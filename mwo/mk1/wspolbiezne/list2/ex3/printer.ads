9bcbcaf8421508ae9801857603b9fea81f640f68
kwziatek <wziatek.karol04@gmail.com>
2025-10-19 18:49:16 +0200

    stop tracking mwo directory

--- mwo/mk1/wspolbiezne/list2/ex3/printer.ads
+++
@@ -1,8 +0,0 @@
-with Parameters; use Parameters;
-with Traces; use Traces;
-
-package Printer is
-   task Printer is
-      entry Report (Traces : Traces_Sequence_Type);
-   end Printer;
-end Printer;
--- mwo/mk1/wspolbiezne/newList4/ex3/readers_writers.ads
+++
@@ -1,8 +0,0 @@
-with Monitor_Package; use Monitor_Package;
-
-package Readers_Writers is
-   procedure Start_Read;
-   procedure Stop_Read;
-   procedure Start_Write;
-   procedure Stop_Write;
-end Readers_Writers;
