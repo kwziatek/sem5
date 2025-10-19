e3c8e9094a22a84cb60ca78885b07bea0bb3d710
kwziatek <wziatek.karol04@gmail.com>
2025-10-19 18:50:27 +0200

    lista1 jfft

--- jfft/lista1/src/FA.java
+++ jfft/lista1/src/FA.java
@@ -8,7 +8,7 @@ public class FA {
         File file = new File(args[1]);
         Scanner scanner = new Scanner(file);
         String word = scanner.nextLine();
-        //System.out.println(KMP(pattern, word, preSuf(pattern)));
+        search(word, pattern);
     }
 
     public static void search(String text, String pattern) {
@@ -27,24 +27,30 @@ public class FA {
 
     private static int[][] buildTransitionTable(String pattern) {
         int m = pattern.length();
-        int R = 128; // assuming ASCII alphabet
+        int R = 128; // ASCII
         int[][] delta = new int[m + 1][R];
 
-        for(int q = 0; q <= m; q++) {
-            for(int c = 0; c < R; c++) {
-
+        for (int q = 0; q <= m; q++) {
+            for (int c = 0; c < R; c++) {
                 int k = Math.min(m, q + 1);
-                while(k > 0 && !matches(pattern, k, (char) c)) {
+                while (k > 0 && !matches(pattern, k, q, (char) c)) {
                     k--;
                 }
-                delta[q][c] = ;
+                delta[q][c] = k;
             }
         }
-        return  delta;
+        return delta;
     }
 
-    private static boolean matches(String pattern, int k, char c) {
-        
+    private static boolean matches(String pattern, int k, int q, char c) {
+        if (k == 0) return true;
+        if (pattern.charAt(k - 1) != c) return false;
+        for (int i = 0; i < k - 1; i++) {
+            if (pattern.charAt(i) != pattern.charAt(q - k + 1 + i)) return false;
+        }
+        return true;
     }
+
+
 
 }
